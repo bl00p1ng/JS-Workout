1a34d91e9b3a0cb61528de66b686c363729c0104
bl00p1ng <blooping@protonmail.com>
2022-05-20 20:12:27 -0500

    Feature parcial: guardar peliculas

--- SeriesAndMoviesTracker/js/app.js
+++ SeriesAndMoviesTracker/js/app.js
@@ -6,14 +6,45 @@ movie.addEventListener('click', showMovieForm);
 serie.addEventListener('click', showSerieForm);
 
 // Mostrar el formulario de registro correspondiente
+// Formulario para registrar película
+const movieForm = document.querySelector('.movie_form')
+// Formulario para registrar película
+const serieForm = document.querySelector('.serie_form')
+
+// Prevenir envio del form
+movieForm.addEventListener('submit', e => e.preventDefault())
+serieForm.addEventListener('submit', e => e.preventDefault())
+
+
 function showMovieForm() {
-    movie.classList.remove('hidden')
-    movie.classList.add('show')
-    console.log(movie.classList);
+    movieForm.classList.remove('hidden')
+    movieForm.classList.add('show')
 }
 
 function showSerieForm() {
-    serie.classList.remove('hidden')
-    serie.classList.add('show')
-    console.log(serie.classList);
+    serieForm.classList.remove('hidden')
+    serieForm.classList.add('show')
 }
+
+// Capturar el texto ingresado en el form
+let db = []  // Almacena los datos registrados
+let movies = []  // Almacena las peliculas
+let series = []  // Almacena las series
+
+// Peliculas
+const registerMovie = document.querySelector('.register-movie')
+
+// Registrar pelicula
+const movieName = document.querySelector('.movie_name')
+const movieStatus = document.querySelector('#seen_status_movie')
+
+registerMovie.addEventListener('click', () => {
+    movies.push({name: movieName.value, movieStatus: movieStatus.value})
+    console.log(movies);
+})
+
+
+// Series
+const serieName = document.querySelector('.serie_name').textContent
+const season = parseInt(document.querySelector('.season').textContent)
+const episode = parseInt(document.querySelector('.episode').textContent)
