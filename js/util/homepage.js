5dbf9fb740b23c04b09335587e7c6062ed41090c
bl00p1ng <blooping@protonmail.com>
2022-07-15 14:33:51 -0500

    Boton flotante

---
+++ js/util/homepage.js
@@ -0,0 +1,7 @@
+// Scripts del home page
+
+// Habilitar el botón flotante
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
+});
